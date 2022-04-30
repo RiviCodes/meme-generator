d868d28e5919a9542c79b5045a84cdbbcc4fc5d6
RiviCodes <jose.riveramz@udlap.mx>
2022-04-30 17:28:37 -0500

    Implement Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,13 @@ import React from "react";
 import "../styles/Header.css";
 
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <header className="header-section">
+        <img src="../images/troll-face.png" className="troll_face" />
+        <h1>Meme Generator</h1>
+        <h2>React Course - Project 3</h2>
+      </header>
+    </>
+  );
 }
